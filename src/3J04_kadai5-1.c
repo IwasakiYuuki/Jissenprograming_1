14b6dd2b7fe78ce2244c1473891b5a8ef954ac05
IwasakiYuuki <yuuki3g@yahoo.co.jp>
2017-07-06 15:15:14 +0900

    create [src/3J04_kadai5-1.c].

---
+++ src/3J04_kadai5-1.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 100
+
+void quicksort(int d[N],int a,int b);
+
+int main(){
+	
+	char filename[256];
+	int data[N],i;
+	FILE *fp;
+
+	printf("pls enter InputFileName...-->");
+	scanf("%s",filename);
+
+	fp=fopen(filename,"r");
+	
+	if(fp==NULL){
+		printf("ERROR : can not open file.\n");
+		exit(1);
+	}
+
+	for(i=0;i<N;i++){
+		fscanf(fp,"%d",&data[i]);
+	}
+	
+
+//	for(i=0;i<N;i++){
+//		printf("%d\n",data[i]);
+//	}
+
+
+	quicksort(data,0,N-1);
+
+	for(i=0;i<N;i++){
+		printf("%d\n",data[i]);
+	}
+
+	return 0;
+}
+
+void quicksort(int d[N],int a,int b){
+	int buf;
+	int first_a,first_b;
+
+	if(a>=b)return;
+	first_a=a;
+	first_b=b;
+
+	while(1){
+		while(a<b&&d[a]<=d[first_a]){
+			a++;
+		}
+		while(a<b&&d[b]>d[first_a]){
+			b--;
+		}
+		if(a>=b)break;
+		buf=d[a];
+		d[a]=d[b];
+		d[b]=buf;
+//		a++;
+//		b--;
+	}
+
+	a--;
+	buf=d[first_a];
+	d[first_a]=d[a];
+	d[a]=buf;
+
+	quicksort(d,first_a,a);
+	quicksort(d,a+1,first_b);
+
+//	for(i=a+1;i<b;i++){
+//		
+//	}
+	return;
+}
