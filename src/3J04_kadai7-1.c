f6ed01f7ecaea6aa98e3e078e0356038e9d20852
IwasakiYuuki <yuuki3g@yahoo.co.jp>
2017-07-13 16:01:55 +0900

    aaa

--- src/3J04_kadai7-1.c
+++ src/3J04_kadai7-1.c
@@ -3,7 +3,10 @@
 
 #define N 1000000
 
+int Cache[N]={0};
+
 void msort(int data[],int left,int right);
+void msort2(int data[],int left,int right);
 
 int main(){
 	
@@ -33,8 +36,10 @@ int main(){
 	for(i=0;i<N;i++){
 		fscanf(fp,"%d",&data[i]);
 	}
+
+
 	printf("[*]Complate loading data.\n");
-	msort(data,0,N-1);
+	msort2(data,0,N-1);
 //	puts("");
 
 	for(i=0;i<N;i++){
@@ -47,6 +52,7 @@ int main(){
 void msort(int data[],int left,int right){
 	
 	int m,buf,i,j,z;
+	register int *cache;
 
 	if(left==right)return;
 
@@ -55,13 +61,45 @@ void msort(int data[],int left,int right){
 	msort(data,m+1,right);
 
 	for(i=m;i>=left;i--){
-		buf=data[i];
+		cache=&data[i];
+		buf=*cache;
 		j=0;
-		while(((i+j)<right)&&(buf>data[i+j+1])){
-			data[i+j]=data[i+j+1];
+		while(((i+j)<right)&&(buf>(*(cache+1)))){
+			*cache=*(cache+1);
+			cache++;
 			j++;
 		}
-		data[i+j]=buf;
+		*(cache)=buf;
 	}
+	return;
+}
+
+void msort2(int data[],int left,int right){
+	
+	int m,i,j,z;
+
+	if(left>=right)return;
+
+	m = (left+right)/2;
+	
+	msort2(data,left,m);
+	msort2(data,m+1,right);
+
+	for(i=left;i<=m;i++)Cache[i]=data[i];
+	for(i=m+1,j=right;i<=right;i++,j--)Cache[i]=data[j];
+
+	i=left;
+	j=right;
+
+	for(z=left;z<=right;z++){
+		if(Cache[i]<=Cache[j]){
+			data[z]=Cache[i];
+			i++;
+		}else{
+			data[z]=Cache[j];
+			j--;
+		}
+	}
+
 	return;
 }
